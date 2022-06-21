5eb68caf811884719e2f688671476c38ef7b2a6e
feilongproject <flzs6137@gmail.com>
2022-06-21 11:08:19 +0800

    refactor: 将stopBot功能移入switch里，优化部分log显示

--- src/index.ts
+++ src/index.ts
@@ -17,43 +17,46 @@ async function main() {
 
     ws.on('PUBLIC_GUILD_MESSAGES', async (data: IntentMessage) => {
         //log.info(mainGuild, typeof mainGuild);
-
+        //log.info(JSON.stringify(data));
         if (data.eventType == 'AT_MESSAGE_CREATE') {
             //log.info(`<@!${meId}>`);
             try {
-                var msg: IMessage = data.msg;
+                var msg: IMessage & IMessageEx = data.msg;
 
                 var messageGuildFoundInGuilds = false;
                 saveGuildsTree.forEach(guild => {
                     if (msg.guild_id == guild.id) {
+                        msg.guild_name = guild.name;
                         log.info(`${guild.name}|||${msg.author.username}|||${msg.content}`), messageGuildFoundInGuilds = true;
                         return;
                     }
                 });
-                if (!messageGuildFoundInGuilds) log.warn(`unKnown message:${msg.guild_id}|||${msg.author.username}|||${msg.content}`);
+                if (!messageGuildFoundInGuilds) log.error(`unKnown message:${msg.guild_id}|||${msg.author.username}|||${msg.content}`);
 
 
-                if (msg.content.includes(stopCommand) && msg.author.username == admin) {
-                    log.info("stoping");
-                    await client.messageApi.postMessage(msg.channel_id, {
-                        content: "ok",
-                        msg_id: msg.id,
-                        message_reference: {
-                            message_id: msg.id,
-                        },
-                    });
-                    process.exit();
-                }
                 //log.debug(msg.channel_id);
                 //log.debug(saveGuilds[3].channel);
                 var content = msg.content.slice(`<@!${meId}>`.length);
                 //log.info(`${content}|`);
 
-                if (findGuilds(saveGuildsTree, msg.guild_id)) {
+                if (findGuilds(saveGuildsTree, msg.guild_id) || msg.author.username == admin) {
 
                     content = content.trim();
                     var userChoice = 0;
                     switch (content) {
+                        case "stopBot":
+                            if (msg.author.username == admin) {
+                                log.info("stoping");
+                                await client.messageApi.postMessage(msg.channel_id, {
+                                    content: "ok",
+                                    msg_id: msg.id,
+                                    message_reference: {
+                                        message_id: msg.id,
+                                    },
+                                });
+                                process.exit();
+                            }
+                            break;
                         case "/单抽出奇迹":
                             commandRand(client, saveGuildsTree, msg, 1);
                             break;
@@ -67,14 +70,14 @@ async function main() {
                             break;
                     }
                 } else {
-                    log.error(`unAuth guild id:${msg.guild_id}|||user:${msg.author.username}`);
+                    log.error(`unAuth guild:${msg.guild_name}(${msg.guild_id})|||user:${msg.author.username}`);
                     sendMsg(client, msg.channel_id, msg.id, "未经授权的操作，请联系机器人管理员获取授权");
                 }
 
 
 
             } catch (error) {
-                log.info(error);
+                log.error(error);
             }
 
         }
