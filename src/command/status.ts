308cba4853ea67bee9dd7b00ed28e15ce162f489
feilongproject <flzs6137@gmail.com>
2022-07-22 17:12:49 +0800

    feat: 添加获取服务器状态命令（简易版）

--- src/command/status.ts
+++ src/command/status.ts
@@ -1,3 +1,4 @@
+import os from "os";
 import { Databaser } from "../mod/databaser";
 import { Messager } from "../mod/messager";
 
@@ -6,7 +7,5 @@ import { Messager } from "../mod/messager";
 
 export async function commandStatus(pusher: Databaser, messager: Messager) {
 
-
-
-    pusher.sendMsg(messager, "");
+    pusher.sendMsg(messager, `系统信息: ${os.release()}\n系统内存: ${(os.freemem() / 1024 / 1024).toFixed()}MB/${(os.totalmem() / 1024 / 1024).toFixed()}MB\n系统已开机: ${os.uptime()}s`);
 }
