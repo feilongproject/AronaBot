0b0c876dd24a809bc50fc7e8cd09418a0a77d0bc
feilongproject <flzs6137@gmail.com>
2022-10-14 12:16:34 +0800

    style: 优化log显示

--- src/plugins/admin.ts
+++ src/plugins/admin.ts
@@ -7,13 +7,13 @@ export async function status(msg: IMessageEx) {
     return msg.sendMsgExRef({
         content: `------状态------` +
             `\n系统版本：${child_process.execSync("lsb_release -d").toString().split(/(\t|\n)/)[2]}` +
-            `\n内核版本：${child_process.execSync("uname --kernel-version").toString().split(/(\t|\n)/)[0]}` +
+            `\n内核版本：${child_process.execSync("uname -a").toString().split(/(\t|\n|\ )/)[4]}` +
             `\n运行时间：${timeConver(new Date().getTime() - global.botStatus.startTime.getTime())}` +
             `\n发送消息：${global.botStatus.msgSendNum}条` +
             `\n生成图片：${global.botStatus.imageRenderNum}次` +
             `\n内存使用：${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)}MB` +
-            `\n系统内存: ${(os.freemem() / 1024 / 1024).toFixed()}MB/${(os.totalmem() / 1024 / 1024).toFixed()}MB` +
-            `\n系统已开机: ${timeConver(os.uptime() * 1000)}`
+            `\n系统内存：${(os.freemem() / 1024 / 1024).toFixed()}MB/${(os.totalmem() / 1024 / 1024).toFixed()}MB (free/total)` +
+            `\n系统已开机：${timeConver(os.uptime() * 1000)}`
     });
 }
 
