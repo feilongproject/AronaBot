e13f50354cdd64e34cfe34519a755974914e37af
feilongproject <flzs6137@gmail.com>
2022-11-22 19:52:12 +0800

    perf: 优化抽卡逻辑，并添加彩奈作为抽卡彩蛋（0.1%概率）

--- src/plugins/gacha.ts
+++ src/plugins/gacha.ts
@@ -5,141 +5,57 @@ import choicesList from "../../data/choices.json";
 
 var maxTime = 30;
 
-export async function gachaHandler(msg: IMessageEx, data: string) {
+export async function gachaString(msg: IMessageEx) {
 
+    const o = cTime(10);
+    var sendStr: string[] = [];
+    if (/十/.test(msg.content)) {
+        sendStr.push(`————————十连结果————————`);
+        for (const value of o) sendStr.push(`(${choicesList.starString[value.star]})(${value.name.source})${value.name.chineseName}`);
+    } else {
+        sendStr.push(`————————单抽结果————————`);
+        sendStr.push(`(${choicesList.starString[o[0].star]})(${o[0].name.source})${o[0].name.chineseName}`);
+    }
+    return msg.sendMsgExRef({ content: sendStr.join(`\n`), }).then(() => {
+        //return redis.setEx(`gachaLimitTTL:${msg.author.id}`, maxTime, "1");
+    });
+}
+
+export async function gachaImage(msg: IMessageEx) {
+
+    if (await hasCd(msg)) return;
+
+    const o = cTime(10);
+
+    /* return msg.sendMarkdown("102024160_1668504873", {
+        at_user: `<@${msg.author.id}>`,
+        today_gacha: "今日x发，共有一星x个，二星x个，三星x个",
+        total_gacha: "累计x发，共有一星x个，二星x个，三星x个",
+        gacha_analyze: "今日出货概率x%，累计出货概率x%",
+        img_size: "img#1700px 980px",
+        img_url: "https://res.feilongproject.com/bot/1665911661651.png",
+    },).then(d => {
+        //log.debug(d);
+    }); */
+    return msg.sendMsgEx({
+        imagePath: await buildImage(o),
+        content: `<@!${msg.author.id}>\n${await analyzeRandData(msg.author.id, o)}`,
+    }).then(() => {
+        return redis.setEx(`gachaLimitTTL:${msg.author.id}`, maxTime, "1");
+    });
+}
+
+async function hasCd(msg: IMessageEx) {
     const ttl = await redis.pTTL(`gachaLimitTTL:${msg.author.id}`);
     const payTTL = parseInt(await redis.hGet(`pay:gachaLimitTTL`, `${msg.author.id}`) || "0");
-    if ((ttl - payTTL > 0) && (msg.author.id != adminId)) {
-        msg.sendMsgEx({
+    if ((ttl - payTTL > 0) && (msg.author.id != adminId))
+        return msg.sendMsgEx({
             content: `请求时间过短，还有${(ttl - payTTL) / 1000}s冷却完毕` +
                 `\n(因为当前服务器性能不足，所以设置冷却cd，赞助以购买一个更好的服务器，也可以获得更少的冷却时间！)` +
                 `\n（当拥有更高配置的服务器时会取消冷却cd限制！）` +
                 `\n（同时为开发者的女装计划助力！）`
         });
-        return;
-    }
-
-    randChoice(msg.author.id, Number(data)).then(result => {
-        return msg.sendMsgExRef(result);
-    }).then(() => {
-        return redis.setEx(`gachaLimitTTL:${msg.author.id}`, maxTime, "1");
-    }).catch(err => {
-        log.error(err);
-    });
-}
-
-
-/**
- * key:   setting:gacha
- * field: ${uid}
- * value: 1(init),0(show/hide),0(continue analyze),0(cut cd (?))
- * @param msg IMessageEx
- * @returns 
- */
-export async function gachaSetting(msg: IMessageEx) {
-
-    if (/重置/.test(msg.content)) {
-        return redis.hSet(`setting:gacha`, `${msg.author.id}`, "1,0,0,0").then(() => {
-            return redis.hSet(`data:allGacha`, `${msg.author.id}`, "0,0,0,0");
-        }).then(() => {
-            return redis.set(`data:todayGacha:${msg.author.id}`, "0,0,0,0");
-        }).then(() => {
-            return msg.sendMsgExRef({
-                content: `已重置卡池设置为默认` +
-                    `\n已重置所有统计为空` +
-                    `\n已重置统计信息为显示状态` +
-                    `\n（这是一个不可撤回的操作！）`,
-            })
-        });
-    }
-
-    if (await redis.hExists(`setting:gacha`, `${msg.author.id}`)) {
-        if (/清空(今日|全部)/.test(msg.content)) {
-            const type = /今日/.test(msg.content) ? "T" : "A";
-            if (type == "T") return redis.set(`data:todayGacha:${msg.author.id}`, "0,0,0,0");
-            else return redis.hSet(`data:allGacha`, `${msg.author.id}`, "0,0,0,0");
-        } else if (/(隐藏|显示)/.test(msg.content)) {
-            const hide = /隐藏/.test(msg.content) ? "1" : "0";
-            const status = await redis.hGet(`setting:gacha`, `${msg.author.id}`);
-            if (!status || !status.startsWith("1")) {
-                return msg.sendMsgExRef({ content: `未找到用户设置，请@bot后输入"/抽卡设置 重置"开始初始化设置` });
-            } else {
-                const join = status.split(",");
-                join[1] = hide;
-                return redis.hSet(`setting:gacha`, `${msg.author.id}`, join.join()).then(() => {
-                    return msg.sendMsgExRef({ content: `已${/(隐藏|显示)/.exec(msg.content)![1]}统计信息` });
-                });
-            }
-        } else if (/帮助/.test(msg.content)) {
-            return msg.sendMsgExRef({
-                content: `抽卡设置 - 帮助界面\n` +
-                    `========================\n` +
-                    `（以下命令必须@机器人后才能使用）\n\n` +
-                    `指令：/抽卡设置 重置\n` +
-                    `介绍：重置所有卡池设置到默认（选择卡池、统计信息等）\n\n` +
-                    `指令：/抽卡设置 清空今日\n` +
-                    `介绍：清空今日抽卡统计信息\n\n` +
-                    `指令：/抽卡设置 清空全部\n` +
-                    `介绍：清空全部抽卡统计信息\n\n` +
-                    `指令：/抽卡设置 隐藏/显示\n` +
-                    `介绍：选择是否隐藏抽卡统计信息（默认显示）`,
-            });
-        } else {
-            return msg.sendMsgExRef({ content: `未知抽卡设置选项，使用"/抽卡设置 帮助"获取指令列表` });
-        }
-    } else {
-        return msg.sendMsgExRef({
-            content: `未找到用户设置，请@bot后输入"/抽卡设置 重置"开始初始化设置` +
-                `\n（如果之后要恢复默认也可使用该命令）`
-        });
-    }
-}
-
-/**
- * 对二进制判断，并返回结果（当未知时返回null）
- * 二进制位数介绍：
- * 第0位：1/0（十连/单抽）
- * 第1位：1/0（图片/文本）
- * @param uid 用户id
- * @param choices 选择条件，以二进制为准
- * @returns 返回本次抽卡结果
- */
-async function randChoice(uid: string, choices: number): Promise<{ content?: string, imagePath?: string }> {
-    //三星角色（彩色卡背）的抽取概率为2.5，二星角色（金色卡背）为18.5，一星角色（灰色卡背）为79
-
-    const o = cTime(choices % 2 ? 10 : 1);
-    switch (choices) {
-        case 0b00://str + 1times
-            return {
-                content: `————————单抽结果————————` +
-                    `\n(${choicesList.starString[o[0].star]})(${o[0].name.source})${o[0].name.chineseName}`
-            };
-
-        case 0b01://str + 10times
-            var content = `————————十连结果————————`;
-            for (const value of o)
-                content += `\n(${choicesList.starString[value.star]})(${value.name.source})${value.name.chineseName}`;
-            return {
-                content: content +
-                    `\n—————————————————————` +
-                    `\n结果仅供娱乐，具体请以实际欧/非气为准` +
-                    `\n图片抽卡功能添加完成，目前可用`
-            };
-
-        case 0b10://image + 1times
-            return { content: `在做了在做了（` };
-
-        case 0b11://image + 10times
-            return {
-                imagePath: await buildImage(o),
-                content: `<@!${uid}>\n${await analyzeRandData(uid, o)}`,
-            };
-
-        default:
-            throw new Error("unkown randChoice's choices");
-
-    }
-
+    return null;
 }
 
 function cTime(times: 1 | 10): { name: Character, star: number }[] {
@@ -165,10 +81,20 @@ function cTime(times: 1 | 10): { name: Character, star: number }[] {
 }
 
 function once(mustStar?: 1 | 2 | 3): { name: Character, star: number } {
-    var rNum = Math.round(Math.random() * 1000);
-    //log.debug(rNum);
+    //三星角色（彩色卡背）的抽取概率为2.5，二星角色（金色卡背）为18.5，一星角色（灰色卡背）为79
     if (mustStar) return { name: second(mustStar), star: mustStar };
-    if (rNum <= 25) {
+
+    var rNum = Math.round(Math.random() * 1000);
+    if (rNum <= 1) {
+        return {
+            name: {
+                source: "Arona",
+                chineseName: "彩奈",
+                fileName: "NPC_Portrait_Arona.png"
+            },
+            star: 3
+        }
+    } else if (rNum <= 25) {
         return { name: second(3), star: 3 };
     } else if (rNum <= 25 + 185) {
         return { name: second(2), star: 2 };
@@ -301,6 +227,72 @@ async function buildImage(characterNames: { name: Character, star: number }[]): 
             });
     }
     return "";
+}
+
+/**
+ * key:   setting:gacha
+ * field: ${uid}
+ * value: 1(init),0(show/hide),0(continue analyze),0(cut cd (?))
+ * @param msg IMessageEx
+ * @returns 
+ */
+export async function gachaSetting(msg: IMessageEx) {
+
+    if (/重置/.test(msg.content)) {
+        return redis.hSet(`setting:gacha`, `${msg.author.id}`, "1,0,0,0").then(() => {
+            return redis.hSet(`data:allGacha`, `${msg.author.id}`, "0,0,0,0");
+        }).then(() => {
+            return redis.set(`data:todayGacha:${msg.author.id}`, "0,0,0,0");
+        }).then(() => {
+            return msg.sendMsgExRef({
+                content: `已重置卡池设置为默认` +
+                    `\n已重置所有统计为空` +
+                    `\n已重置统计信息为显示状态` +
+                    `\n（这是一个不可撤回的操作！）`,
+            })
+        });
+    }
+
+    if (await redis.hExists(`setting:gacha`, `${msg.author.id}`)) {
+        if (/清空(今日|全部)/.test(msg.content)) {
+            const type = /今日/.test(msg.content) ? "T" : "A";
+            if (type == "T") return redis.set(`data:todayGacha:${msg.author.id}`, "0,0,0,0");
+            else return redis.hSet(`data:allGacha`, `${msg.author.id}`, "0,0,0,0");
+        } else if (/(隐藏|显示)/.test(msg.content)) {
+            const hide = /隐藏/.test(msg.content) ? "1" : "0";
+            const status = await redis.hGet(`setting:gacha`, `${msg.author.id}`);
+            if (!status || !status.startsWith("1")) {
+                return msg.sendMsgExRef({ content: `未找到用户设置，请@bot后输入"/抽卡设置 重置"开始初始化设置` });
+            } else {
+                const join = status.split(",");
+                join[1] = hide;
+                return redis.hSet(`setting:gacha`, `${msg.author.id}`, join.join()).then(() => {
+                    return msg.sendMsgExRef({ content: `已${/(隐藏|显示)/.exec(msg.content)![1]}统计信息` });
+                });
+            }
+        } else if (/帮助/.test(msg.content)) {
+            return msg.sendMsgExRef({
+                content: `抽卡设置 - 帮助界面\n` +
+                    `========================\n` +
+                    `（以下命令必须@机器人后才能使用）\n\n` +
+                    `指令：/抽卡设置 重置\n` +
+                    `介绍：重置所有卡池设置到默认（选择卡池、统计信息等）\n\n` +
+                    `指令：/抽卡设置 清空今日\n` +
+                    `介绍：清空今日抽卡统计信息\n\n` +
+                    `指令：/抽卡设置 清空全部\n` +
+                    `介绍：清空全部抽卡统计信息\n\n` +
+                    `指令：/抽卡设置 隐藏/显示\n` +
+                    `介绍：选择是否隐藏抽卡统计信息（默认显示）`,
+            });
+        } else {
+            return msg.sendMsgExRef({ content: `未知抽卡设置选项，使用"/抽卡设置 帮助"获取指令列表` });
+        }
+    } else {
+        return msg.sendMsgExRef({
+            content: `未找到用户设置，请@bot后输入"/抽卡设置 重置"开始初始化设置` +
+                `\n（如果之后要恢复默认也可使用该命令）`
+        });
+    }
 }
 
 interface GachaSetting {
