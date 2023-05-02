ab892fbfed04fa027819987b4636bcd44e9bf4d0
feilongproject <flzs6137@gmail.com>
2023-05-02 18:35:10 +0800

    perf: 优化游戏服务器状态回复, 新增资源

--- src/plugins/serverStatus.ts
+++ src/plugins/serverStatus.ts
@@ -9,6 +9,13 @@ export async function baServerStatus(msg: IMessageGUILD) {
     // const settingServer = (await settingUserConfig(msg.author.id, "GET", ["server"])).server as "jp" | "global" | undefined;
 
     return fetch("https://prod-noticeindex.bluearchiveyostar.com/prod/index.json").then(res => res.json()).then((json: ServerStatusJP) => {
+        // log.debug(json.Maintenance.StartDate)
+        const endTime = new Date(json.Maintenance.EndDate);
+        if (endTime.getTime() + 1000 * 60 * 60 <= new Date().getTime()) {
+            return msg.sendMsgEx({
+                content: "日服一切正常, 暂无官方维护通知(以游戏内提示为准)",
+            });
+        }
 
         return msg.sendMsgEx({
             content: `日服状态:` +
@@ -77,12 +84,3 @@ interface ServerStatusJP {
         Text: string;
     };
 }
-
-
-/* 
-https://prod-noticeindex.bluearchiveyostar.com/prod/index.json
-.Maintenance
-
-
-
-*/
