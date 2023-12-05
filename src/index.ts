5a88345104e1866c82eadfeb5a0732b917645967
feilongproject <flzs6137@gmail.com>
2023-12-05 23:25:26 +0800

    perf: 优化监听逻辑

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import config from "../config/config";
 
 init().then(() => {
     for (const eventRootType of config.bots[botType].intents) {
-        global.ws.on(eventRootType, async (data: IntentMessage.GUILD_MESSAGES) => {
+        log.mark(`开始监听 ${eventRootType} 事件`);
+        global.ws.on(eventRootType, async (data: IntentMessage.EventRespose<any>) => {
             data.eventRootType = eventRootType;
             return import("./eventRec").then(e => e.eventRec(data));
         });
