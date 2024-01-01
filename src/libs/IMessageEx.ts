e93057340ac636ce1a395c954d3e67851692e99a
feilongproject <flzs6137@gmail.com>
2024-01-01 18:00:38 +0800

    feat: 添加关于random图片的发送逻辑

--- src/libs/IMessageEx.ts
+++ src/libs/IMessageEx.ts
@@ -251,14 +251,15 @@ export class IMessageGROUP extends IMessageChatCommon implements IntentMessage.G
         const fileMap = [[], ["png", "jpg", "jpeg"], ["mp4"], ["slik"]];
         options.fileType = fileMap.findIndex(v => v.find(i => uT.startsWith(i))) as any || undefined;
         if ((options.fileType as number) == -1) options.fileType = undefined;
-        if (options.fileType || options.fileUrl) options.msgType = 7;
+        if (options.fileType || options.fileUrl || options.imageUrl) options.msgType = 7;
+        if ((options.fileUrl || options.imageUrl)?.endsWith("/random")) options.fileType = 1;
         return callWithRetry(this._sendMsgEx, [options]) as any;
     }
 
     private _sendMsgEx = async (options: Partial<SendOption.Group>) => {
         const { content, groupId, msgType, msgId, ark } = options;
         if (!groupId) throw "not has groupId";
-        const fileInfo = msgType == 7 ? await this._sendFile(options) : null;
+        const fileInfo = msgType == 7 ? await this._sendFile(options, (options.fileUrl || options.imageUrl)?.endsWith("/random")) : null;
         return client.groupApi.postMessage(groupId, {
             content: content ? ("\n" + content) : "",
             msg_type: msgType || 0,
@@ -284,7 +285,7 @@ export class IMessageGROUP extends IMessageChatCommon implements IntentMessage.G
         return callWithRetry(this._sendFile, [options, force]);
     }
 
-    private _sendFile = async (options: Partial<SendOption.Group>, force: boolean = false): Promise<string> => {
+    private _sendFile = async (options: Partial<SendOption.Group>, force = false): Promise<string> => {
         const { imageUrl, fileUrl, groupId, fileType } = options;
         const fUrl = imageUrl || fileUrl;
         if (!groupId) throw "not has groupId";
@@ -298,10 +299,10 @@ export class IMessageGROUP extends IMessageChatCommon implements IntentMessage.G
         if (!new URL(fUrl).pathname.startsWith("/p/gacha/")) log.mark(`资源 ${fUrl} 获取中, 存在: ${!!fileInfo}`);
         const fileRes = await client.groupApi.postFile(groupId, {
             file_type: fileType,
-            url: fUrl,
+            url: fUrl + (fUrl.endsWith("/random") ? `?rat=111&ts=${new Date().getTime()}` : ""),
             srv_send_msg: false,
         }).then(res => res.data);
-        await redis.setEx(redisKey, 60 * 60 * 24 * 5, fileRes.file_info);
+        if (!force) await redis.setEx(redisKey, 60 * 60 * 24 * 5, fileRes.file_info);
         return fileRes.file_info;
     }
 
